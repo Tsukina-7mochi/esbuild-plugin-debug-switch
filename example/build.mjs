2bdac27b886c1d89b271cc4a18c3df2df0fe5409
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-10-24 21:11:57 +0900

    docs: update example

--- example/build.mjs
+++ example/build.mjs
@@ -1,19 +1,29 @@
-import * as esbuild from 'esbuild';
-import { debugSwitchPlugin } from 'esbuild-plugin-debug-switch/plugin';
+import * as esbuild from "esbuild";
+import { debugSwitchPlugin } from "esbuild-plugin-debug-switch/plugin";
 
 await Promise.all([
   esbuild.build({
-    entryPoints: ['./src/main.ts'],
-    outfile: './dist/main.prod.js',
+    entryPoints: ["./src/main.ts"],
+    outfile: "./dist/main.prod.js",
     bundle: true,
     minify: true,
-    plugins: [debugSwitchPlugin({ isDebug: false })],
+    plugins: [
+      debugSwitchPlugin({
+        isDebug: false,
+        env: { version: "1.0.0" },
+      }),
+    ],
   }),
   esbuild.build({
-    entryPoints: ['./src/main.ts'],
-    outfile: './dist/main.dev.js',
+    entryPoints: ["./src/main.ts"],
+    outfile: "./dist/main.dev.js",
     bundle: true,
     minify: true,
-    plugins: [debugSwitchPlugin({ isDebug: true })],
+    plugins: [
+      debugSwitchPlugin({
+        isDebug: false,
+        env: { version: "1.0.0" },
+      }),
+    ],
   }),
 ]);
--- example/src/main.ts
+++ example/src/main.ts
@@ -1,7 +1,8 @@
-import { isDebug } from 'esbuild-plugin-debug-switch';
+import { env, isDebug } from "esbuild-plugin-debug-switch";
 
 if (isDebug) {
-  console.log('This is development mode!');
+  console.log("This is development mode!");
 } else {
-  console.log('This is production mode!');
+  console.log("This is production mode!");
 }
+console.log(`The version is ${env.version}`);
